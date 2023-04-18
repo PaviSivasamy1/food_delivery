d2c6455a6922796237b613f259acd47ac2689c56
Pavithira Sivasamy <pavithira.sivasamy@lvchn.com>
2023-04-18 18:40:39 +0530

    code base added

--- order_m_service/src/main/java/com/example/order_m_service/model/Order.java
+++ order_m_service/src/main/java/com/example/order_m_service/model/Order.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
     private String orderID;
-    private String items;
-    private String qty;
+    private List<String> items;
+    private List<String> qty;
     private Long price;
 }
